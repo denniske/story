e558c417fd801fa537ad4ce1fe7d3326222a081f
Dennis Keil <dennis.keil10@gmail.com>
2024-11-20 14:53:08 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,31 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config'
+import storyblok from '@storyblok/astro'
+import {loadEnv} from 'vite'
+import tailwind from "@astrojs/tailwind"
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://astro.build/config
-export default defineConfig({});
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
+
+export default defineConfig({
+    integrations: [
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            apiOptions: {
+                region: 'eu',
+            },
+            components: {
+                page: 'storyblok/Page',
+                feature: 'storyblok/Feature',
+                grid: 'storyblok/Grid',
+                teaser: 'storyblok/Teaser',
+            },
+        }),
+        tailwind()
+    ],
+    vite: {
+        plugins: [basicSsl()],
+        server: {
+            https: true,
+        },
+    },
+})
